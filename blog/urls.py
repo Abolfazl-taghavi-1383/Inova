4f52af38bf2c941058e87efd1da17003a180b8a9
Ablfxl <ablfxlll@gmail.com>
2025-12-05 02:57:11 +0330

    feat(blog): create posts list and post detail api endpoints

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import ListPostAPIView, DetailPostAPIView
+
+app_name = "posts"
 
 urlpatterns = [
-    
+    path("", ListPostAPIView.as_view(), name="list_post"),
+    path("<str:slug>/", DetailPostAPIView.as_view(), name="post_detail"),
 ]
