112fe5a9500e275a8a637d29592425eaf28429ef
Ablfxl <ablfxlll@gmail.com>
2025-12-05 03:49:03 +0330

    feat(blog): create universal image serve endpoint

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ListPostAPIView, DetailPostAPIView
+from .views import ListPostAPIView, DetailPostAPIView, serve_universal_image
 
-app_name = "posts"
+app_name = "blog"
 
 urlpatterns = [
     path("", ListPostAPIView.as_view(), name="list_post"),
     path("<str:slug>/", DetailPostAPIView.as_view(), name="post_detail"),
+    path('media/<str:model_type>/<uuid:pk>/', serve_universal_image, name='universal_image_serve'),
 ]
