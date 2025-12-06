1d1a292da2c9206ab55ca00f077f7846dff13bc1
Ablfxl <ablfxlll@gmail.com>
2025-12-06 10:22:23 +0330

    feat(blog): customize  admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -5,4 +5,6 @@ from .views import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    readonly_fields = ['slug'] 
+    list_display = ('title', 'created_at', 'updated_at', 'slug')
+    search_fields = ('title', )
+    readonly_fields = ('slug', ) 
